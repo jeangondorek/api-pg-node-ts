5699f4c0f0f8595ac57d3445ba81bdc1e73801ab
jeangondorek <jeangondorek73@gmail.com>
2023-05-10 12:05:54 +0000

    update

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express from 'express';
 
 const server = express();
 
-server.get('/', (req, res)=>{
-    return res.send('hi');
-})
+interface Teste{
+    
+}
 
+server.get('/', (req, res)=>{
+	return res.send('hi');
+});
 
 export {server};
