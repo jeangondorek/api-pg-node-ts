6a0d4f699e85ddc6484180154bdf50a51d1496f2
Jean Carlos Canova Gondorek <jean.gondorek@expressosaomiguel.com.br>
2023-05-15 14:01:59 -0300

    config to pg

--- src/server/database/knex/KnexIndex.ts
+++ src/server/database/knex/KnexIndex.ts
@@ -1,6 +1,11 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { knex } from 'knex';
+import pg from 'pg';
 import { development, production, test } from './Environment';
+
+if (process.env.NODE_ENV === 'production'){
+	pg.types.setTypeParser(20, 'text', parseInt);
+}
 
 const getEnv = () =>{
 	switch(process.env.NODE_ENV){
