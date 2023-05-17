14ca0069d97b894b359b0fab24aac96d0f16cb52
Jean Carlos Canova Gondorek <jean.gondorek@expressosaomiguel.com.br>
2023-05-17 10:40:19 -0300

    addeded swagger in /api-docs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -6,7 +6,23 @@ import './shared/services/TranslationsYup';
 
 import { router } from './routes/RoutesIndex';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
 const server = express();
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+	explorer: true,
+	swaggerOptions: {
+		securityDefinitions: {
+			bearerAuth: {
+				type: 'apiKey',
+				name: 'Authorization',
+				in: 'header',
+				description: 'Enter the token in the following format: Bearer {token}'
+			}
+		}
+	}}));
 
 server.use(cors({
 	origin: process.env.ENABLED_CORS?.split(';') || []
